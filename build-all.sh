2cbc3820ec8c5e1916edd799030435b1b8437171
wmannard <wmannard@coveo.com>
2025-08-11 11:59:21 -0400

    add meta tags

--- build-all.sh
+++ build-all.sh
@@ -2,7 +2,7 @@
 
 for JAMBOREE in {1..9}; do
   for LOCALE in EN FR; do
-    OUTDIR="dist/jamboree_${JAMBOREE}_${LOCALE,,}"
+    OUTDIR="dist/jamboree_${JAMBOREE}_$(echo $LOCALE | tr '[:upper:]' '[:lower:]')"
     echo "Building for jamboree $JAMBOREE $LOCALE → $OUTDIR"
     VITE_JAMBOREE=$JAMBOREE VITE_LOCALE=$LOCALE npx vite build --outDir $OUTDIR &
   done
